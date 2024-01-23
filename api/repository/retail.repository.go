da20b49299f2f9bb1a7e416a8ec0800418b471a3
unknown <111202214715@mhs.dinus.ac.id>
2024-01-23 17:55:36 +0700

    create method to delete Retail in repository file

--- api/repository/retail.repository.go
+++ api/repository/retail.repository.go
@@ -3,22 +3,24 @@ package repository
 import (
 	"BagasA11/go-POS/api/models"
 	"BagasA11/go-POS/configs"
+	"time"
 
 	"gorm.io/gorm"
 )
 
+/*Define Retail Repositry structure*/
 type RetailRepository struct {
 	db *gorm.DB
 }
 
-// initializing repository
+/*create Retail Repository instance*/
 func NewRetailRepository() *RetailRepository {
 	return &RetailRepository{
 		db: configs.GetDB(),
 	}
 }
 
-// create new record
+/*create new record*/
 func (retailRepo *RetailRepository) CreateRetail(retail models.Retail) error {
 	tx := retailRepo.db.Begin()
 	err := tx.Create(&retail).Error
@@ -29,7 +31,7 @@ func (retailRepo *RetailRepository) CreateRetail(retail models.Retail) error {
 	return err
 }
 
-// find by ID
+/*find Retail object by ID*/
 func (retailRepo *RetailRepository) FindByID(ID uint) (models.Retail, error) {
 	var rtl models.Retail
 
@@ -38,14 +40,51 @@ func (retailRepo *RetailRepository) FindByID(ID uint) (models.Retail, error) {
 	return rtl, err
 }
 
-// get retail accounts from entire db record
-func (rtlRepo *RetailRepository) FindAll() ([]models.Retail, error) {
-	var rtl []models.Retail
-
-	err := rtlRepo.db.Find(&rtl).Error
-
+/*find retail by email attribute*/
+func (rtlRepo *RetailRepository) FindEmail(Email string) (models.Retail, error) {
+	var rtl models.Retail
+	err := rtlRepo.db.Where("email = ?", Email).First(&rtl).Error
 	return rtl, err
 }
 
-// change Retail account attribute
-func Update() {}
+/*retrieve entire record of Retail object*/
+func (rtlRepo *RetailRepository) FindAll() ([]models.Retail, error) {
+	var rtl []models.Retail
+	err := rtlRepo.db.Find(&rtl).Error
+	return rtl, err
+}
+
+/*change Retail account attribute*/
+func (rtlRepo *RetailRepository) Update(rtl *models.Retail) error {
+	tx := rtlRepo.db.Begin()
+	err := tx.Model(&models.Retail{}).Where("id = ?", rtl.ID).Updates(&rtl).Error
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	tx.Commit()
+	return nil
+}
+
+/*decrease cashier quota where new cashier employee is registered*/
+func (rtlRepo *RetailRepository) DecreaseCashier(rtl *models.Retail) error {
+	tx := rtlRepo.db.Begin()
+	err := tx.Model(&models.Retail{}).Where("id = ?", rtl.ID).Error
+	if err != nil {
+		return err
+	}
+	rtl.CashierAvb -= 1
+	rtlRepo.db.Save(&rtl)
+	return nil
+}
+
+/*delete Retail record*/
+func (rtlRepo *RetailRepository) Delete(ID uint) error {
+	tx := rtlRepo.db.Begin()
+	err := tx.Model(&models.Retail{}).Where("id = ?", ID).Update("deleted_at", time.Now().Unix()).Error
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	return nil
+}
