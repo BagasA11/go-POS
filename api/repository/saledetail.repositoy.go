3196ce2132712882920f9cc216a0b81688bf66ee
unknown <111202214715@mhs.dinus.ac.id>
2024-01-25 22:00:48 +0700

    Delete method for Sale Detail

--- api/repository/saledetail.repositoy.go
+++ api/repository/saledetail.repositoy.go
@@ -45,3 +45,34 @@ func (SdtRepo *SaleDetailRepo) FindID(id uint) (models.SaleDetail, error) {
 	err := SdtRepo.Db.Where("id = ?", id).First(&sdt).Error
 	return sdt, err
 }
+
+/*Get All Sales Detail data*/
+func (SdtRepo *SaleDetailRepo) All() ([]models.SaleDetail, error) {
+	var sdt []models.SaleDetail
+	err := SdtRepo.Db.Find(&sdt).Error
+	return sdt, err
+}
+
+/*UPdate Sales Detail Data*/
+func (SdtRepo *SaleDetailRepo) Update(sdt *models.SaleDetail) error {
+	tx := SdtRepo.Db.Begin()
+	err := tx.Model(&models.SaleDetail{}).Where("id = ?", sdt.ID).Updates(&sdt).Error
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	tx.Commit()
+	return nil
+}
+
+/*Delete Sales Detail record*/
+func (SdtRepo *SaleDetailRepo) Delete(id uint) error {
+	tx := SdtRepo.Db.Begin()
+	// DELETE FROM saledetails WHERE id = {id};
+	err := tx.Delete(&models.SaleDetail{}, id).Error
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	return nil
+}
