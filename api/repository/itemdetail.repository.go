e4d6f9f36938cdd71516d4f57e23a3bde562acb6
unknown <111202214715@mhs.dinus.ac.id>
2024-01-25 22:29:14 +0700

    itemdetail.repository.go

--- api/repository/itemdetail.repository.go
+++ api/repository/itemdetail.repository.go
@@ -1,1 +1,61 @@
 package repository
+
+import (
+	"BagasA11/go-POS/api/models"
+	"BagasA11/go-POS/configs"
+
+	"gorm.io/gorm"
+)
+
+type ItemDetailRepository struct {
+	Db *gorm.DB
+}
+
+func NewItmDtlRepo() *ItemDetailRepository {
+	return &ItemDetailRepository{
+		Db: configs.GetDB(),
+	}
+}
+
+// all
+//uuid
+//soldout
+//update
+//delete
+
+// Create new Item Detail
+func (ItDtlRepo *ItemDetailRepository) Create(itmDtl models.ItemDetail) error {
+	tx := ItDtlRepo.Db.Begin()
+	err := tx.Create(&itmDtl).Error
+	if err != nil {
+		return err
+	}
+	tx.Commit()
+	return nil
+}
+
+// Get All Item Detail Data
+func (ItDtlRepo *ItemDetailRepository) All() ([]models.ItemDetail, error) {
+	var itmDtl []models.ItemDetail
+	err := ItDtlRepo.Db.Find(&itmDtl).Error
+	return itmDtl, err
+}
+
+/*find data by uuid*/
+func (ItDtlRepo *ItemDetailRepository) Uuid(uuid string) (models.ItemDetail, error) {
+	var itdtl models.ItemDetail
+	err := ItDtlRepo.Db.Where("uuid = ?", uuid).First(&itdtl).Error
+	return itdtl, err
+}
+
+/*Sold Out method change avail property to false*/
+func (ItDtlRepo *ItemDetailRepository) SoldOut(itmDtl models.ItemDetail) error {
+	tx := ItDtlRepo.Db.Begin()
+	err := tx.Model(&models.ItemDetail{}).Where("uuid = ?", itmDtl.UUID).Update("avail", false).Error
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	tx.Commit()
+	return nil
+}
