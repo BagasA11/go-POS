630b50df2bd70e6fd47a2feb030de837f9e0734d
unknown <111202214715@mhs.dinus.ac.id>
2024-01-25 08:07:46 +0700

    enable Create and FindByID utilitize for Sale class

--- api/repository/sale.repository.go
+++ api/repository/sale.repository.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"BagasA11/go-POS/api/models"
 	"BagasA11/go-POS/configs"
 
 	"gorm.io/gorm"
@@ -10,8 +11,27 @@ type SaleRepository struct {
 	Db *gorm.DB
 }
 
+/*Sale Repository instance*/
 func NewSaleRepository() *SaleRepository {
 	return &SaleRepository{
 		Db: configs.GetDB(),
 	}
+}
+
+/*create new sale record*/
+func (SlRepo *SaleRepository) Create(sale models.Sale) error {
+	tx := SlRepo.Db.Begin()
+	err := tx.Create(&sale).Error
+	if err != nil {
+		return err
+	}
+	tx.Commit()
+	return err
+}
+
+/*find sales by Id*/
+func (SlRepo *SaleRepository) FindByID(id uint) (models.Sale, error) {
+	var sale models.Sale
+	err := SlRepo.Db.Where("id = ?", id).First(&sale).Error
+	return sale, err
 }
