88f2d2b052f1cdc24ff9ec828b569633fc143bef
unknown <111202214715@mhs.dinus.ac.id>
2024-01-25 17:47:33 +0700

    sale detail repository

--- api/repository/saledetail.repositoy.go
+++ api/repository/saledetail.repositoy.go
@@ -1,1 +1,17 @@
 package repository
+
+import (
+	"BagasA11/go-POS/configs"
+
+	"gorm.io/gorm"
+)
+
+type SaleDetailRepo struct {
+	Db *gorm.DB
+}
+
+func NewSaleDetailRepo() *SaleDetailRepo {
+	return &SaleDetailRepo{
+		Db: configs.GetDB(),
+	}
+}
