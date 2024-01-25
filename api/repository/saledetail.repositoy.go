ef9e9c4527850e8e9bcfb69c7481deeaf04f66a1
unknown <111202214715@mhs.dinus.ac.id>
2024-01-25 18:06:40 +0700

    Find data by id method on saledetail.repository.go file

--- api/repository/saledetail.repositoy.go
+++ api/repository/saledetail.repositoy.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"BagasA11/go-POS/api/models"
 	"BagasA11/go-POS/configs"
 
 	"gorm.io/gorm"
@@ -14,4 +15,33 @@ func NewSaleDetailRepo() *SaleDetailRepo {
 	return &SaleDetailRepo{
 		Db: configs.GetDB(),
 	}
+}
+
+/*Create method to insert new data*/
+func (SdtRepo *SaleDetailRepo) Create(sdt models.SaleDetail) error {
+	tx := SdtRepo.Db.Begin()
+	err := tx.Create(&sdt).Error
+	if err != nil {
+		return err
+	}
+	tx.Commit()
+	return nil
+}
+
+/*create multiple record*/
+func (SdtRepo *SaleDetailRepo) CreateMany(sdts []models.SaleDetail) error {
+	tx := SdtRepo.Db.Begin()
+	err := tx.Create(&sdts).Error
+	if err != nil {
+		return err
+	}
+	tx.Commit()
+	return nil
+}
+
+/*Find Sale Detail data using id parameter*/
+func (SdtRepo *SaleDetailRepo) FindID(id uint) (models.SaleDetail, error) {
+	var sdt models.SaleDetail
+	err := SdtRepo.Db.Where("id = ?", id).First(&sdt).Error
+	return sdt, err
 }
